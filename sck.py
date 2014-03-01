2b225266a9828ff8588e971c511a63adc3900f1b
Dougal Scott <dougal.scott@gmail.com>
2014-03-02 10:55:36 +1100

    Added /all option

--- sck.py
+++ sck.py
@@ -20,9 +20,15 @@ def status():
     data = getStats()
     return render_template('status.html', data=data['devices'][0]['posts'])
 
+###############################################################################
+@app.route("/all")
+def all():
+    data = getStats()
+    return render_template('all.html', data=data['devices'][0]['posts'])
+
 
 ###############################################################################
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=5000)
 
 #EOF
